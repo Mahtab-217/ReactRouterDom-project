7aad4da8cd929a487a16e26cb23c62944e7ac9b6
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-09 22:42:16 +0430

    new style

--- src/Testing.jsx
+++ src/Testing.jsx
@@ -5,11 +5,14 @@ export default function(){
     
     return(
         <div>
-            <h1 className="text-5xl">
+            <h1 className="text-5xl text-center">
                 0
             </h1>
+            <p className="
+            font-bold text-2xl font-bold
+            ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores magni eligendi ullam exercitationem sed similique! Distinctio neque dolorem consequuntur magnam. Quos similique ratione quidem reprehenderit dolorum recusandae molestias, at repellat.</p>
             <hr />
-            <button className="bg-orange-700 py-2 px-5" onClick={()=>setqimat(qimat+1)}></button>
+            <button className="bg-orange-700 jutify-center py-2 px-5" onClick={()=>setqimat(qimat+1)}></button>
         </div>
     )
 }
