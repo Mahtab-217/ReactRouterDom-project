85c5c13ef86d3f039d4b83aa0a42c7d0e58871b3
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-10 21:56:27 +0430

    new code

--- src/Testing.jsx
+++ src/Testing.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react"
 
 export default function(){
-    const [qimat,setqimat]=useState(0);
+    let [qimat,setqimat]=useState(0);
     useEffect(greet, [qimat]);
+    let [list, setList]=useState([]); 
     function greet(){
-        console.log("Hello")
+        console.log("The value changed")
     }
     return(
         <div>
-            <h1 className="text-5xl text-center">
+            {list.length ===0 &&(
+                <div>
+                    <h1>No user exists</h1>
+                </div>
+            )}
+            {/* <h1 className="text-5xl text-center">
                 {qimat}
             </h1>
             <button className="py-2 px-5 bg-orange-600 rounded-md" onClick={()=>setqimat(qimat+1)}>Add Number </button>
             <hr />
-            <button className="bg-orange-700 jutify-center py-2 px-5" onClick={()=>setqimat(qimat+1)}></button>
+            <button className="bg-orange-700 jutify-center py-2 px-5" onClick={()=>setqimat(qimat+1)}></button> */}
         </div>
     )
 }
